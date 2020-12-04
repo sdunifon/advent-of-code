aab227e14aff6a86d6f9d1cb15e81b630921eb22
Scott Dunifon <sdunifon@gmail.com>
2020-12-04 17:05:55 -0500

    adding sled to day 3

--- src/bin/3/main.rs
+++ src/bin/3/main.rs
@@ -1,13 +1,12 @@
 #![feature(min_const_generics)]
 
-use std::fmt::Display;
-
 use input::INPUT;
+use std::fmt::Display;
 
 mod input;
 
 fn main() {
-    let forest = Forest::new(INPUT);
+    let forest = Forest::new(INPUT.to_vec());
 
     println!("{}", forest);
 }
@@ -18,30 +17,92 @@ struct Forest<'a> {
 }
 
 #[derive(Debug)]
-struct ForestLine<'a>(&'a str);
+struct ForestLine<'a> {
+    spaces: Vec<Space>,
+    string: &'a str,
+}
 
-// 🌲⬜ ⚪⬛
 impl<'a> Forest<'a> {
     fn new(input: Vec<&'a str>) -> Forest {
         Forest {
-            lines: input.iter().map(|line| ForestLine(line)).collect(),
+            lines: input.iter().map(|line| ForestLine::new(line)).collect(),
         }
+    }
+    fn end_height(&self) -> u32 {
+        self.lines.len() as u32
     }
 }
 
 impl<'a> std::fmt::Display for Forest<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let output = self.lines.join("\n");
+        let output = self
+            .lines
+            .iter()
+            .fold("".to_string(), |acc, line| format!("{}\n{}", acc, line));
         write!(f, "{}", output)
     }
 }
 impl<'a> ForestLine<'a> {
+    fn new(string: &str) -> ForestLine {
+        ForestLine {
+            spaces: string.chars().map(|c| Space::new(c)).collect(),
+            string,
+        }
+    }
     fn emoji_output(&self) -> String {
-        self.0.replace(".", "⬛").replace("#", "🌲")
+        // self.0.replace(".", "⬛").replace("#", "🌲")
+        self.spaces
+            .iter()
+            .map(|space| space.to_string())
+            .collect::<String>()
     }
 }
 impl<'a> Display for ForestLine<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "-{}-", self.emoji_output())
+        write!(f, "|{}|", self.emoji_output())
     }
+}
+
+#[derive(Debug)]
+struct Space {
+    tree: bool,
+}
+
+impl Space {
+    fn new(c: char) -> Space {
+        Space {
+            tree: match c {
+                '#' => true,
+                _ => false,
+            },
+        }
+    }
+}
+impl Display for Space {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c = match self.tree {
+            true => "🌲",
+            false => "⬛",
+        };
+        write!(f, "{}", c)
+    }
+}
+
+struct Sled {
+    tree_hits: u32,
+    position: (u32, u32),
+    delta_pos: (u32, u32),
+    forest: Forest<'static>,
+}
+
+impl Sled {
+    fn new(delta_pos: (u32, u32), forest: Forest<'static>) -> Sled {
+        Sled {
+            delta_pos,
+            forest,
+            position: (0, 0),
+            tree_hits: 0,
+        }
+    }
+    fn slide() {}
 }
